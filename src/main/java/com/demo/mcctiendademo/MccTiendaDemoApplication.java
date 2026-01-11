f5d1b3b20310392116a05e6a79aff68328de5f79
roberto <roberto.mosquera.dev@gmail.com>
2026-01-11 16:51:55 -0500

    Initial commit - Spring Boot API Tienda Demo

---
+++ src/main/java/com/demo/mcctiendademo/MccTiendaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mcctiendademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccTiendaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MccTiendaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/mcctiendademo/MccTiendaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mcctiendademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MccTiendaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
